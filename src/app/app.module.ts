9b39cff9ba4d587aecf1bf72c3b4f13172634235
Brian <brian.d.lobo@gmail.com>
2021-11-10 09:32:22 -0300

    Se chorean estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { SpinnerComponent } from './shared/spinner.component';
+import { NavigationComponent } from './shared/header-navigation/navigation.component';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    SpinnerComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PerfectScrollbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
