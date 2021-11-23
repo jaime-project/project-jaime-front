35dc082ba050da3c10bc59f4a7a9ca7dcaae505e
Brian <brian.d.lobo@gmail.com>
2021-11-23 09:00:46 -0300

    Se agrega modal al eliminar

--- src/app/components/servers/list/list.component.ts
+++ src/app/components/servers/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-list-server',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListServerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
+  }
+
+  openVerticallyCentered(content3: string) {
+    this.modalService.open(content3, { centered: true });
   }
 
 }
