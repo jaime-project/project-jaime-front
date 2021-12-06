8adfbf92b5307c10de4ae7bfdd8a48b713b937ba
Brian <brian.d.lobo@gmail.com>
2021-12-06 09:56:33 -0300

    Se mejora el edit module

--- src/app/components/modules/docs/docs.component.ts
+++ src/app/components/modules/docs/docs.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocsModuleComponent implements OnInit {
 
+  public docsEditSwitchActivated = false
+
+
   constructor() { }
 
   ngOnInit(): void {
