3b6c346b7e1512306874267f921dad68b1705f7a
Brian <asd>
2021-12-11 12:43:12 -0300

    Se hacen cambios

---
+++ src/app/components/works/logs/log.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-log-work',
+  templateUrl: './log.component.html',
+  styleUrls: ['./log.component.css']
+})
+export class LogWorkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
