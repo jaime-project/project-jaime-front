7f7d424300d7d9fe8e3f68bb8225e2330c41109f
Brian <asd>
2021-12-14 01:15:38 -0300

    Se deja funcionando los docs

---
+++ src/app/services/modules/docs.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DocsService {
+
+  apiUrl: string = environment.backendURL + '/api/v1/modules/docs';
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+  constructor(private http: HttpClient) { }
+
+  getDocs(name: string | null): Observable<string> {
+    const headers = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
+
+    return this.http.get(this.apiUrl + '/' + name, { headers, responseType: 'text' })
+      .pipe(
+        catchError(this.httpError)
+      )
+  }
+
+  deleteDocs(name: string | null): Observable<any> {
+    return this.http.delete<any>(this.apiUrl + '/' + name)
+      .pipe(
+        catchError(this.httpError)
+      )
+  }
+
+  putDocs(name: string | null, content: string): Observable<any> {
+    const headers = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
+    return this.http.put(this.apiUrl + '/' + name, content, { headers })
+      .pipe(
+        catchError(this.httpError)
+      )
+  }
+
+  httpError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(errorMessage);
+  }
+}
