0ad52cd2f9f6f19efab05cb4cb4d921068eddc81
Brian <brian.d.lobo@gmail.com>
2022-08-12 14:37:17 -0300

    Se agrega edit job

--- src/app/components/works/yaml/yaml.component.ts
+++ src/app/components/works/yaml/yaml.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { WorkService } from 'src/app/services/works/work.service';
-import { Document } from 'yaml';
+import Swal from 'sweetalert2';
+import { Document, parse } from 'yaml';
 
 @Component({
   selector: 'app-yaml-work',
@@ -10,19 +11,44 @@ import { Document } from 'yaml';
 })
 export class YamlWorkComponent implements OnInit {
 
+  public editSwitchActivated = false
+
   contentYaml: string = ""
 
-  constructor(private workService: WorkService, private route: ActivatedRoute) { }
+  work_id: string = ""
+
+  constructor(private workService: WorkService, private activatedRoute: ActivatedRoute, private route: Router) { }
 
   ngOnInit(): void {
-    let id = this.route.snapshot.paramMap.get('id')
+    this.work_id = this.activatedRoute.snapshot.paramMap.get('id')!
 
-    this.workService.getWork(id).subscribe(data => {
+    this.workService.getWork(this.work_id).subscribe(data => {
 
       let doc = new Document()
       doc.contents = data
       this.contentYaml = doc.toString()
     })
+  }
+
+
+  putWork(modifyYaml: string) {
+
+    Swal.fire({
+      title: 'Update Job',
+      text: 'Update job with id "' + this.work_id + '"',
+      icon: 'warning',
+      confirmButtonColor: '#05b281',
+      cancelButtonColor: '#ec312d',
+      showCancelButton: true,
+    }).then(result => {
+      if (result.isConfirmed) {
+        this.workService.putWork(parse(modifyYaml))
+          .subscribe(() =>
+            this.route.navigate(['works'])
+          )
+      }
+    })
+
   }
 
 
