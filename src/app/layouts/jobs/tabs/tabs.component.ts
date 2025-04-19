200e3027e076a102036b789254ff2a9ff66edb1c
brianlobo-procloud <brian.lobo@procloudengineers.com>
2025-04-19 15:09:27 -0300

    Se agrega el estatus en los logs del job

--- src/app/layouts/jobs/tabs/tabs.component.ts
+++ src/app/layouts/jobs/tabs/tabs.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { interval, Subscription } from 'rxjs';
 import { JobService } from 'src/app/services/jobs/job.service';
 
 @Component({
@@ -11,17 +12,35 @@ export class TabsJobComponent implements OnInit {
 
   selectedTabId = 1
   jobName: string = ""
+  jobStatus: string = ""
+  thread: Subscription | null = null
 
   constructor(private jobService: JobService, private route: ActivatedRoute) { }
 
+
   ngOnInit(): void {
 
+    this.loadStartData()
+
+    this.thread = interval(1000)
+      .subscribe(() => {
+        this.loadStartData()
+      });
+
+
+  }
+
+
+  loadStartData() {
     let jobId = this.route.snapshot.paramMap.get('id') as string
 
     this.jobService.getJob(jobId)
       .subscribe(data => {
         this.jobName = data.name
+        this.jobStatus = data.status
       })
   }
+
+
 
 }
