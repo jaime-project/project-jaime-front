9b169a7c036752003ee67c62aba66233836c77e7
brianlobo-procloud <brian.lobo@procloudengineers.com>
2025-04-23 12:30:03 -0300

    Se corrije login

--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -1,7 +1,9 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { AppConfigService } from '../AppConfigService';
+import { ErrorService } from '../errors/error.service';
 
 
 @Injectable({
@@ -12,11 +14,16 @@ export class LoginService {
   apiUrl: string = "";
   headers = new HttpHeaders().set('Content-Type', 'application/json');
 
-  constructor(private environment: AppConfigService, private http: HttpClient) {
+  constructor(private environment: AppConfigService, private http: HttpClient, private errorService: ErrorService) {
     this.apiUrl = environment.config.backendURL + '/api/v1/login/';
   }
 
   login(user: string, password: string): Observable<any> {
     return this.http.post(this.apiUrl, { 'user': user, 'password': password }, { responseType: 'text' })
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          return this.errorService.httpError(error);
+        })
+      )
   }
 }
